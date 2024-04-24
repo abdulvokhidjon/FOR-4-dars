58f583b3cbd5894965a4e67d0766da8e384be456
abdulvokhidjon <162565383+abdulvokhidjon@users.noreply.github.com>
2024-04-24 21:42:26 +0500

    added

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 FOR
-// N{1}
-// let k = +prompt("Iltimos, k sonini kiriting:");
-// let n = +prompt("Iltimos, n sonini kiriting:");
+N{1}
+let k = +prompt("Iltimos, k sonini kiriting:");
+let n = +prompt("Iltimos, n sonini kiriting:");
 
-// // For tsikli orqali k sonini n marta chiqaramiz
-// for (let i = 0; i < n; i++) {
-//     console.log(k);
-// }
+// For tsikli orqali k sonini n marta chiqaramiz
+for (let i = 0; i < n; i++) {
+    console.log(k);
+}
 
 // N{2}
 // const a = +prompt("Marhamat qilib birinchi qiymatni kiriting (a):");
